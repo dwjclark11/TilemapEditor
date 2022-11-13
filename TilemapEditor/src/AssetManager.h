de2a1b72ba8acad85fbbedf3e0637bcf5b02edff
Dustin Clark <dwjclark1@hotmail.com>
2022-11-13 13:19:56 -0500

    Fixed Comment

--- TilemapEditor/src/AssetManager.h
+++ TilemapEditor/src/AssetManager.h
@@ -21,9 +21,9 @@ public:
 
 	/*
 	*	AddTexture - Creates a texture from an SDL_Surface and adds it to the texture map
-	*	@param: SDL_Renderer for creating the texture
-	*	@param: std::string assetId - The key for the texture in the map
-	*	@param: std::string filepath - Where the image is located to create the texture
+	*	@param SDL_Renderer for creating the texture
+	*	@param std::string assetId - The key for the texture in the map
+	*	@param std::string filepath - Where the image is located to create the texture
 	*/
 	void AddTexture(Renderer& renderer, const std::string& assetId, const std::string& filePath);
 	const Texture& GetTexture(const std::string& assetId);
